781b159225078d6bf886eb4443171d7971f650d0
AaradhyaParsodia <aaradhyaparsodia@gmail.com>
2024-09-21 19:53:31 +0530

    Initialise the repo

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/check", (req, res) => {
+    res.status(200).send("<h1> All Good <h1>");
+});
+
+app.use('/*', (req, res)=>{
+    res.status(200).send("<h1> Ohhhh ho there is no way ahead<h1>");
+});
+
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send({ message: "Internal Server Error" });
+});
+
+// connectionDB();
+
+app.listen(port, (err)=>{
+    if(!err){
+        console.log(`shhhhhhhhhhhhhh it is listening over ${port} using [Express]`);
+    }
+    else{
+        console.error(err);
+    }
+});
